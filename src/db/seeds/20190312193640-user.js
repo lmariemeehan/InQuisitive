1911699e64e8e323740693deab7e37594998b617
Lola <lmariemeehan@gmail.com>
2019-03-12 19:48:32 -0700

    Completed seeding data assignment

---
+++ src/db/seeds/20190312193640-user.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const faker = require("faker");
+let users = [];
+
+for(let i =1; i <= 15; i++) {
+  users.push({
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    role: 0,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  });
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert("Users", users, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete("Users", null, {});
+  }
+};
---
+++ src/db/seeds/20190312193706-wiki.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const faker = require("faker");
+let wikis = [];
+
+for(let i = 1; i <= 20; i++) {
+  wikis.push({
+    title: faker.hacker.noun(),
+    body: faker.hacker.phrase(),
+    private: faker.random.boolean(),
+    userId: 5,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+}
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert("Wikis", wikis, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete("Wikis", null, {});
+  }
+};
