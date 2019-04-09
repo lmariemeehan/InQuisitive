36c7c489bcb57c51a4a157d64bf3624eb7c0ac8f
Lola <lmariemeehan@gmail.com>
2019-04-09 14:00:16 -0700

    Edited server.js file

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const app = require("./app");
 const http = require("http");
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
-const server = http.createServer(app);
 
 server.listen(port);
 
