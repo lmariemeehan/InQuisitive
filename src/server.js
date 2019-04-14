78e0e75d127407ce7f86b18266b23b22f3973323
Lola <lmariemeehan@gmail.com>
2019-04-13 21:16:08 -0700

    Hiding env file

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 const server = http.createServer(app);
+
 server.listen(port);
 
 function normalizePort(val) {
