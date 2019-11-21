74d10f8f3a4373f7e6ead8b1aba7f615944c2806
Lola <lmariemeehan@gmail.com>
2019-11-20 23:34:33 -0800

    Added icons and working on adding a profile page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -33,7 +33,7 @@ module.exports = {
 				res.redirect("/users/sign_up");
 			} else {
 				passport.authenticate("local")(req, res, () => {
-					req.flash("notice", "You've successfully signed in!");
+					req.flash("notice", "You've successfully signed up!");
 					res.redirect("/");
 				})
 			}
